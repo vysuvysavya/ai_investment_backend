8c5d666ae946bc464a126af3b30cc72cc6507526
vysavya <vysu.vysavya@gmail.com>
2025-03-09 12:55:25 +0530

    /

--- investment-backend/ai-service/model_api.py
+++ investment-backend/ai-service/model_api.py
@@ -7,6 +7,10 @@ app = FastAPI()
 
 model = joblib.load("inv_rec.pkl")
 
+@app.get("/")
+def home():
+    return {"message": "AI Service is running!"}
+
 @app.post("/predict")
 def predict(data: dict):
     df = pd.DataFrame([data])
