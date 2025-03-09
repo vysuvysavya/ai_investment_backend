1b54db9f981bcde5d36aae8e66540260c0373e18
vysavya <vysu.vysavya@gmail.com>
2025-03-09 13:18:10 +0530

    pool

--- investment-backend/backend/db.js
+++ investment-backend/backend/db.js
@@ -1,5 +1,8 @@
-require("dotenv").config();
-import { Pool } from "pg";
+import dotenv from "dotenv";
+import pkg from "pg";
+
+dotenv.config();
+const { Pool } = pkg;
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
